850dbaba14948b514314bf67c921b8ab17985bcc
Pawel Szostak <szostak_pawel@outlook.com>
2020-05-12 19:10:51 +0200

    Updates

---
+++ precache-manifest.2241573a4f705597f75fac520920c611.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6fae143f85a6269fb24060c759268df",
+    "url": "/tetris-react/index.html"
+  },
+  {
+    "revision": "c27b086aa351b950a18e",
+    "url": "/tetris-react/static/css/main.e68a0767.chunk.css"
+  },
+  {
+    "revision": "7b511120b39b26428a0b",
+    "url": "/tetris-react/static/js/2.498de994.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tetris-react/static/js/2.498de994.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c27b086aa351b950a18e",
+    "url": "/tetris-react/static/js/main.8447d696.chunk.js"
+  },
+  {
+    "revision": "66a15fb403661766758b",
+    "url": "/tetris-react/static/js/runtime-main.5cb8f77e.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/tetris-react/static/media/Pixel.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/tetris-react/static/media/bgc.2810fe6b.png"
+  }
+]);
